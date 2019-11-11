fabf14db412c1fb94bfcf4facef946d66256db5d
Chavoy Davis <chavoy.davis3@gmail.com>
2019-11-10 23:59:10 -0500

    Order History

---
+++ HondaXpress/OrderHistory.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/User.Master" AutoEventWireup="true" CodeBehind="OrderHistory.aspx.cs" Inherits="HondaXpress.OrderHistory" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div id="div" style="color: #FFFFFF; background-color: #C0C0C0;">
+    <asp:PlaceHolder ID = "PlaceHolder1" runat="server" />
+        </div>
+</asp:Content>
---
+++ HondaXpress/OrderHistory.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HondaXpress
+{
+    public partial class OrderHistory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                //Populating a DataTable from database.
+                DataTable dt = this.GetData();
+
+                //Building an HTML string.
+                StringBuilder html = new StringBuilder();
+
+                //Table start.
+                html.Append("<table border = '1'>");
+
+                //Building the Header row.
+                html.Append("<tr>");
+                foreach (DataColumn column in dt.Columns)
+                {
+                    html.Append("<th>");
+                    html.Append(column.ColumnName);
+                    html.Append("</th>");
+                }
+                html.Append("</tr>");
+
+                //Building the Data rows.
+                foreach (DataRow row in dt.Rows)
+                {
+                    html.Append("<tr>");
+                    foreach (DataColumn column in dt.Columns)
+                    {
+                        html.Append("<td>");
+                        html.Append(row[column.ColumnName]);
+                        html.Append("</td>");
+                    }
+                    html.Append("</tr>");
+                }
+
+                //Table end.
+                html.Append("</table>");
+
+                //Append the HTML string to Placeholder.
+                PlaceHolder1.Controls.Add(new Literal { Text = html.ToString() });
+            }
+        }
+
+        private DataTable GetData()
+        {
+
+            if (Session["USERNAME"] != null)
+            {
+                string USER = Session["USERNAME"].ToString();
+
+                string constr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    using (SqlCommand cmd = new SqlCommand("SELECT * FROM tblOrder where Username= @User", con)) 
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Parameters.AddWithValue("@User", USER);
+                        using (SqlDataAdapter sda = new SqlDataAdapter())
+                        {
+                           
+                            sda.SelectCommand = cmd;
+                            using (DataTable dt = new DataTable())
+                            {
+                                sda.Fill(dt);
+                                return dt;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Response.Redirect("~/Login.aspx");
+                return null;
+            }
+        }
+    }
+
+}
